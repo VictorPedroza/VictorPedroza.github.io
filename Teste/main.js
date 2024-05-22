5bdb91e7f77e34e651a6000086beda6066a8132d
VictorPedroza <161649329+VictorPedroza@users.noreply.github.com>
2024-05-22 17:08:58 -0300

    Add files via upload

---
+++ Teste/main.js
@@ -0,0 +1,9 @@
+let num1 = 1234
+let num2 = 2312
+
+
+if (typeof(num1) == typeof(num2)) {
+    console.log('num1 tem o mesmo tipo de num2')
+} else {
+    console.log('num1 não tem o mesmo tipo de num2')
+}
