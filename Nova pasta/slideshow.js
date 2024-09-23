15d4f8adc0b927fa56cee40fee785f306d80d49a
VictorPedroza <161649329+VictorPedroza@users.noreply.github.com>
2024-09-23 10:32:03 -0300

    Delete Nova pasta directory

--- Nova pasta/slideshow.js
+++
@@ -1,9 +0,0 @@
-// script.js
-const slidebarImage = document.getElementById('slidebar-image');
-const imagePaths = ['./img/Calleri.jpg', './img/Rogero.jpg', './img/Torcida.jpg', './img/Torcida1.jpg'];
-let currentImageIndex = 0;
-
-slidebarImage.addEventListener('click', () => {
-    currentImageIndex = (currentImageIndex + 1) % imagePaths.length;
-    slidebarImage.src = imagePaths[currentImageIndex];
-});
